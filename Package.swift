6cf6b84f10fcf96394eef7df53ce49f92c755740
Jeroen Desloovere <info@jeroendesloovere.be>
2017-12-28 11:19:05 +0100

    targets must preceed dependencies

--- Package.swift
+++ Package.swift
@@ -8,20 +8,20 @@ let package = Package(
         .library(name: "App", targets: ["App"]),
         .executable(name: "Run", targets: ["Run"])
     ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor", "LeafProvider"],
+                exclude: [
+                    "Config",
+                    "Database",
+                    "Public",
+                    "Resources"
+            ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["Vapor", "LeafProvider"],
-               exclude: [
-                   "Config",
-                   "Database",
-                   "Public",
-                   "Resources"
-               ]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
 
